d060c98d327557a70c027027dc4b949a4a52d5ad
Akshat <akshatvijayvergiya64@gmail.com>
2024-07-04 12:12:03 +0530

    Initial Commit

---
+++ src/LoginValidation.js
@@ -0,0 +1,13 @@
+import LogIn from "./pages/login";
+
+function validation($){
+    alert("")
+    let error ={};
+    const email_pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    const password_pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$/
+
+    
+    
+}
+
+export default validation;

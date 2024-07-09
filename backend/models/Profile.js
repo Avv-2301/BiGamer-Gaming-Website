e97f8e181170c5447ceca373b2c20986104e3636
Akshat <akshatvijayvergiya64@gmail.com>
2024-07-09 16:13:17 +0530

    Initial Commit

---
+++ backend/models/Profile.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const profileSchema = new mongoose.Schema(
+    {
+        gender:{
+            type:String
+        },
+        dateOfBirth:{
+            type:String
+        },
+        contactNumber:{
+            type:Number,
+        }
+    }
+)
+
+module.exports = mongoose.model("Profile", profileSchema);

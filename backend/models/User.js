71efb6f132a007aa4545977d31755ff1346b5a80
Akshat <akshatvijayvergiya64@gmail.com>
2024-07-09 14:52:01 +0530

    Initial Commit

---
+++ backend/models/User.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+
+const UserSchema = new mongoose.Schema(
+    {
+        firstName:{
+            type:String,
+            required:true
+        },
+        lastName:{
+            type:String,
+            required:true
+        },
+        email:{
+            type:String,
+            required:true
+        },
+        password:{
+            type:String,
+            required:true,
+        },
+        accountType:{
+            type:String,
+            enum:["Admin"],
+            required:true
+        },
+        profile:{
+            type:mongoose.Schema.Types.ObjectId,
+            required:true,
+            ref:"Profile"
+        },
+        games:[{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Games"
+            
+        }],
+        image:{
+            type:String,
+            required:true
+        },
+        token:{
+            type:String
+        },
+        resetPasswordExpires:{
+            type:Date
+        }
+    },
+    {timestamps: true}
+)
+
+
+module.exports = mongoose.model("User",UserSchema);

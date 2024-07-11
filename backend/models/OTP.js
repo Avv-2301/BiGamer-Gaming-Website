f822079a04cf4ad21e4766c902f2353a2b97b4ae
Akshat <akshatvijayvergiya64@gmail.com>
2024-07-11 23:49:24 +0530

    Initial Commit

--- backend/models/OTP.js
+++ backend/models/OTP.js
@@ -1,22 +1,46 @@
 const mongoose = require("mongoose");
 
-const OTPSchema = new mongoose.Schema(
-    {
-        email:{
-            type:String,
-            required:true
-        },
-        otp:{
-            type:String,
-            required:true
-        },
-        createdAt:{
-            type:Date,
-            default:Date.now,
-            expires:5*60
-        }
+const OTPSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  otp: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    expires: 5 * 60,
+  },
+});
+
+
+async function sendVerificationEmail(email, otp){
+    try{
+        const mailResponse = await mailSender(
+            email,
+            "Verification Email",
+            otp
+        )
+        console.log("Email Response", mailResponse.response)
     }
-)
+    catch(error){
+        console.log("Error while sending email",error)
+        throw error;
+    }
+}
 
 
-module.exports = mongoose.model("OTP",OTPSchema);
+
+OTPSchema.pre("save", async function(next){
+	console.log("New document saved to database");
+
+        if(this.isNew){
+            await sendVerificationEmail(this.email, this.otp)
+        }
+        next();
+})
+
+module.exports = mongoose.model("OTP", OTPSchema);

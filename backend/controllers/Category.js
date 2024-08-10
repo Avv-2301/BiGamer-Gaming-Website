b707f53f7a79e062c6c44353cf6c8fe766ce7f2b
Akshat <akshatvijayvergiya64@gmail.com>
2024-08-10 22:59:11 +0530

    Category Controller

--- backend/controllers/Category.js
+++ backend/controllers/Category.js
@@ -1,1 +1,29 @@
-const Category = require('../models/Category');
+const Category = require('../models/CategoryModel');
+
+exports.createCategory = async(req, res) =>{
+    try{
+        const {categoryName} = req.body;
+
+        if(!categoryName){
+            return res.status(401).json({
+                success:false,
+                message:'CategoryName is required'
+            })
+        }
+        const category = await Category.create({
+            name:categoryName
+        })
+        console.log(category);
+        return res.status(200).json({
+            success:true,
+            message:'Category created successfully'
+        })
+    }
+    catch(error){
+        console.log(error);
+        return res.status(500).json({
+            success:false,
+            message: error.message,
+        })
+    }
+}

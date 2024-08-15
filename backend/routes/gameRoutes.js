2f9baeeea29a12167c39b246da80f97573a73873
Akshat <akshatvijayvergiya64@gmail.com>
2024-08-15 21:28:53 +0530

    Games Controller

--- backend/routes/gameRoutes.js
+++ backend/routes/gameRoutes.js
@@ -3,13 +3,14 @@ const router = express.Router();
 
 const { createCategory, showCategory } = require('../controllers/Category.js')
 const { auth, isAdmin } = require('../middlewares/AuthMidddleware');
-const {createGame} = require('../controllers/Games.js');
+const {createGame, getAllGames} = require('../controllers/Games.js');
 
 router.post('/createcategory',createCategory, auth, isAdmin);
 router.get('/showcategory',showCategory);
 
 
 router.post('/creategame', auth, isAdmin, createGame);
+router.get('/allgames', getAllGames);
 
 
 module.exports = router;
